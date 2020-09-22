6dcb0ec2d695a64f05fe1e909e3cdf161ec6b4d0
Torsten Pause <torsten.pause@student.kyh.se>
2020-09-22 17:46:24 +0200

    adding some missing code

--- src/com/company/Patient.java
+++ src/com/company/Patient.java
@@ -1,8 +1,24 @@
 package com.company;
 
 class Patient {
+    private String name;
+    private String sickness;
+    private String medicin;
+    private Boolean stillSick;
 
-    public void isSick(){
+    public String getName() { return name; }
+    public String getSickness() { return sickness; }
+
+    public boolean isSick(){ return stillSick; }
+
+
+
+
+    public void takeMedication(String med) {
+     //inte rätt med mot åkomma sätt  ändrar stillSick till sant eller falskt
 
     }
 }
+
+
+//Patienten ska ha fyra stycken medlemsmetoder och två stycken konstruktorer.
