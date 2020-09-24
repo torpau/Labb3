8721732ae2ae862c208106d1ad5bf8aa68226970
Torsten Pause <torsten.pause@student.kyh.se>
2020-09-24 16:27:35 +0200

    achieved functional program

--- src/com/company/Patient.java
+++ src/com/company/Patient.java
@@ -6,19 +6,43 @@ class Patient {
     private String medicin;
     private Boolean stillSick;
 
+
+
+
+    Patient(String name) {
+        this(name, "");
+    }
+
+    Patient(String name, String sickness) {
+        this.name = name;
+        this.sickness = sickness;
+        //this.medicin = medicin;
+        this.stillSick = true;
+    }
+
+
+
     public String getName() { return name; }
     public String getSickness() { return sickness; }
 
-    public boolean isSick(){ return stillSick; }
+    public boolean isSick() {
+        if(sickness==null||sickness.equals("")) {                    //ev ta bort else
+            return false;
+        }
+        return true;
+    }
 
+     public void takeMedication(Medicine medicin) {
+         if(medicin.getTreatmentName().equals(this.sickness)){
+             sickness = "";
+         }
 
-
-
-    public void takeMedication(String med) {
      //inte rätt med mot åkomma sätt  ändrar stillSick till sant eller falskt
+        //Medicine(String sickness);
 
     }
 }
 
 
-//Patienten ska ha fyra stycken medlemsmetoder och två stycken konstruktorer.
+//Patienten ska ha fyra stycken medlemsmetoder och två stycken konstruktorer(skapa objektet med dess attribut).
+
